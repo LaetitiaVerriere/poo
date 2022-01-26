5e4e2a9b22d97f2df8e10ae7d38ca3e4eb600e27
Laetitia Verriere <laetitia.verriere@gmail.com>
2022-01-26 10:03:24 +0100

    mise à jour application

--- src/banque/Rib.java
+++ src/banque/Rib.java
@@ -1,11 +1,12 @@
 package banque;
-import javax.swing.JOptionPane;
+
 
 public class Rib {
-	Account account;
-	public static void main(String[] args) {
-		Account account  =new Account();
-		JOptionPane.showMessageDialog(null,  account.messageBank + "\n" + account.dot +"\n" + "\n" + account.messageCustomer + "\n" + account.dot + "\n" + account.iban +"\n"+ account.dot + "\n" + account.code  );
-	}
+
+		 Account account  =new Account();
+		 public String concatMessage =  account.messageBank + "\n" + account.dot +"\n" + "\n" + account.messageCustomer + "\n" + account.dot + "\n" + account.iban +"\n"+ account.dot + "\n" + account.code;
+		 public void afficher( String message){
+			 System.out.println(message);
+					 }
 
 }
---
+++ src/banque/RibTest.java
@@ -0,0 +1,10 @@
+package banque;
+
+public class RibTest {
+
+	public static void main(String[] args) {
+		Rib rib = new Rib();
+		rib.afficher(rib.concatMessage);
+	}
+
+}
